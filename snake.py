8745d5678305c01879261a403955e00df0aff531
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-06-10 19:57:35 -0400

    nice

--- snake.py
+++ snake.py
@@ -22,66 +22,14 @@ class Head:
         self.y = self.y + self.speed
 
 #define window
-class Window:
-    def __init__(self):
-        self._running = True
-        self._display_surf = None
-        self._image_surf = None
-        self.windowWidth = 800
-        self.windowHeight = 600
-        self.player = Player() 
 
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
-        
-        pygame.display.set_caption('Pygame pythonspot.com example')
-        self._running = True
-        self._image_surf = pygame.image.load("pygame.png").convert()
- 
-    def on_event(self, event):
-        if event.type == QUIT:
-            self._running = False
-
-    def on_loop(self):
-        pass
-    
-    def on_render(self):
-        self._display_surf.fill((0,0,0))
-        self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
-        pygame.display.flip()
- 
-    def on_cleanup(self):
-        pygame.quit()
- 
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
- 
-        while( self._running ):
-            pygame.event.pump()
-            keys = pygame.key.get_pressed() 
-            
-            if (keys[K_RIGHT]):
-                self.player.moveRight()
-
-            if (keys[K_LEFT]):
-                self.player.moveLeft()
-
-            if (keys[K_UP]):
-                self.player.moveUp()
-
-            if (keys[K_DOWN]):
-                self.player.moveDown()
-
-            if (keys[K_ESCAPE]):
-                self._running = False
-
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
- 
 #main loop
-if __name__ == "__main__" :
-    theWindow = Window()
-    theWindow.on_execute()
+while not done:
+
+
+
+    #render
+    screen.fill((0,0,0))
+
+
+    pygame.display.flip()
