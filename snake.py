0672c515c479c7a048c48008ca81e8077377c12a
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-06-10 20:03:32 -0400

    hey

--- snake.py
+++ snake.py
@@ -8,18 +8,32 @@ class Head:
         self.x=x
         self.y=y
         self.speed=speed
+        self.direction = 0
+
+    def update(self):
+        if self.direction == 0:
+            self.x = self.x + self.speed
+        if self.direction == 1:
+            self.x = self.x - self.speed
+        if self.direction == 2:
+            self.y = self.y - self.speed
+        if self.direction == 3:
+            self.y = self.y + self.speed
 
     def moveRight(self):
-        self.x = self.x + self.speed
+        self.direction = 0
 
     def moveLeft(self):
-        self.x = self.x - self.speed
+        self.direction = 1
 
     def moveUp(self):
-        self.y = self.y - self.speed
+        self.direction = 2
 
     def moveDown(self):
-        self.y = self.y + self.speed
+        self.direction = 3
+    
+    #render script
+    def draw()
 
 #define window
 
