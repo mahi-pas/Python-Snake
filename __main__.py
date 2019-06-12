9be805987352dacbe59b68e8c024911e30e18f9a
Mahisaurus-Rex <mahipasarkar@gmail.com>
2019-06-12 09:18:09 -0400

    Added structure; returns error on draw()

--- __main__.py
+++ __main__.py
@@ -1,3 +1,30 @@
 import pygame
 import time
 import random
+from snake import *
+
+#initialize
+pygame.init()
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30) #change font sometime
+screen = pygame.display.set_mode((1280,720))
+done = False
+
+#initial variables
+p1_x,p1_y=30,30
+#make objects
+p1=Snake(p1_x,p1_y)
+
+#main loop
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pressed=pygame.key.get_pressed()
+    #Set controls p1
+    
+
+    #render frame
+    screen.fill((0,0,0))
+    p1.draw(screen)
+    pygame.display.flip()
--- snake.py
+++ snake.py
@@ -2,29 +2,27 @@ import pygame
 import time
 import random
 
-#make head
-class Head:
-    def __init__(self,x,y,speed):
+#snake class
+class Snake:
+    #Macros
+    len=30
+
+    def __init__(self,x,y):
         self.x=x
         self.y=y
-        self.speed=speed
-        self.direction = 0
         self.length=3
-        self.updateCountMax = 2
-        self.updateCount = 0
-            for i in range(0,length):
-        self.x.append(0)
-        self.y.append(0)
+        self.dir=0
+        self.speed=2
 
     def update(self):
         if self.direction == 0:
-            self.x = self.x + self.speed
+            self.x += self.speed
         if self.direction == 1:
-            self.x = self.x - self.speed
+            self.x -= self.speed
         if self.direction == 2:
-            self.y = self.y - self.speed
+            self.y -= self.speed
         if self.direction == 3:
-            self.y = self.y + self.speed
+            self.y += self.speed
 
     def moveRight(self):
         self.direction = 0
@@ -37,21 +35,7 @@ class Head:
 
     def moveDown(self):
         self.direction = 3
-    
-    #render script
-    def draw(self, surface, image):
-        for i in range(0,self.length):
-            surface.blit(image,(self.x[i],self.y[i])) 
 
-#define window
-
-#main loop
-while not done:
-
-
-
-    #render
-    screen.fill((0,0,0))
-
-
-    pygame.display.flip()
+    def draw(self,screen):
+        pygame.draw.rect(screen, (255,255,255), pygame.Rect(self.x,self.y,len,len))
+        self.update()
